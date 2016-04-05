b56cfa4dbba649b7712963c05432c8d9a681f470
Jean Gonçalves <jeanfpgoncalves@live.com>
2016-04-04 21:46:37 -0300

    Abrir job da tarefa ao clicar na notificação e fechar a notificação

--- script.js
+++ script.js
@@ -30,7 +30,6 @@
 
     var verifyStorage = function(json) {
         var arrData = [];
-        var arrStorage;
         for (var i = 0; i < json.length; i++) {
             arrData.push(json[i].Tasks.iTask);
         }
@@ -42,7 +41,7 @@
                         var body = json[i].Tasks.sTask;
                         var description = 'Job: ' + json[i].ViewCmpRelatedDocuments.sDocumentNumber + ' '+ json[i].ViewCmpRelatedDocuments.sDocument;
 
-                        notifyMe('Nova Tarefa', body +'\n'+ description, 'task_'+arrData[i]);
+                        notifyMe('Nova Tarefa', body +'\n'+ description, 'task_'+arrData[i], linkUrl + json[i].Tasks.sDoc + '/' + json[i].Tasks.iDoc);
                         delete arrTask[arrTask.Tasks.indexOf(arrData[i])];
                     }
                 }
@@ -55,7 +54,7 @@
         chrome.storage.local.set({'Tasks': data});
     };
 
-    var notifyMe = function(sTitle, sBody, sTag) {
+    var notifyMe = function(sTitle, sBody, sTag, sUrl) {
         if ("Notification" in window) {
             if (Notification.permission === "granted") {
                 var notification =  new Notification(sTitle, {
@@ -63,6 +62,13 @@
                         body: sBody,
                         tag: sTag
                     });
+                if (sUrl !== undefined) {
+                    notification.onclick = function() {
+                      event.preventDefault();
+                      window.open(sUrl, '_blank');
+                      notification.close();
+                    }
+                }
                 
             }
         }
